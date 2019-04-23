139dc14c04737dc26a6f7aab4df471a71037281e
1 <g.ponsard@simplon-charleville.fr>
2019-04-23 16:50:04 +0200

    .

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>exo_php</title>
+</head>
+
+<body>
+    <?php
+/******* exo 1): afficher le plus petit entre A et B *******/
+
+
+/*
+$a = 4;
+$b = 6;
+if ($a > $b) {
+    echo $b;
+}
+else ($a < $b) {
+    echo $a;
+}*/
+
+
+/******* exo 2): afficher le plus petit entre A, B et C *******/
+
+/*$a = 5;
+$b = 4;
+$c= 3;
+
+echo min ($a, $b, $c);*/
+
+
+/******** exo 3): afficher un nombre aléatoire entre 1 et 100; *********/
+
+/*echo rand (2,99);*/
+
+
+/******** exo 4): afficher un nombre aléatoire entre 5 et 100 **********/
+
+/*echo rand (6,99);*/
+
+
+
+/******** exo 5): Faire une fonction additon **********/
+
+/*function addition($n1,$n2){
+    echo $n1+$n2;
+}
+addition(14,7);*/
+
+
+/********* exo 6): faire une fonction PGCD ***********/
+
+/*** bwahhhhhh ****/
+
+
+/********* exo 7): Afficher "Je vais faire du bon visuel" 
+ *                 si x (<-- nb aléatoire entre 1 et 100) < 50 
+ *                 sinon "Je suis un piou piou"; ******************/
+
+ 
+
+
+?>
+</body>
+
+</html>
